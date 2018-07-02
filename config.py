92458b9e1ab51ebc4bb9c89355e3f180014cba77
Alex Kazakov <moonsly@gmail.com>
2018-07-02 03:03:07 +0300

    fixes

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ TXN_CONFIRMATIONS = 12
 infura_url = "https://mainnet.infura.io/{}".format(INFURA_API_KEY)
 transfer_topic = HexBytes('0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef')
 
-db_name, db_user, db_pass = "septemex", "septemex", "septemex"
+db_name, db_user, db_pass = "", "", ""
 conn_string = "host='localhost' dbname='{}' user='{}' password='{}'".format(db_name, db_user, db_pass)
